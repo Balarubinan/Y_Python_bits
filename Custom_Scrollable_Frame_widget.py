180907d656755c5df6d6c53492a2aeada35bd42a
Bala <sudhagouthirubi@gmail.com>
2020-10-04 18:30:01 -1200

    Added file for tiled,tabbedwidget and FTP file server
    Signed-off-by: Bala <sudhagouthirubi@gmail.com>

--- Custom_Scrollable_Frame_widget.py
+++ Custom_Scrollable_Frame_widget.py
@@ -9,12 +9,12 @@ class ScrollableFrame(Frame):
         self.last_end = 0
         self.spacing = spacing
         self.margin = margin
-        self.canv = Canvas(self, bg='lightgreen', height='500', width='500', scrollregion=(0, 0, 0, 0))
+        self.canv = Canvas(self, bg='lightgreen', height='300', width='500', scrollregion=(0, 0, 0, 1000))
 
         # to use scrollbar
-        # self.scrollbar = Scrollbar(self, orient="vertical", command=self.canv.yview)
-        # self.canv.configure(yscrollcommand=self.scrollbar.set)
-        # self.scrollbar.pack(side="right", fill="y")
+        self.scrollbar = Scrollbar(self, orient="vertical", command=self.canv.yview)
+        self.canv.configure(yscrollcommand=self.scrollbar.set)
+        self.scrollbar.pack(side="right", fill="y")
 
         self.canv.pack()
         self.canv.bind_all("<MouseWheel>", self.on_mouse_wheel)
@@ -24,10 +24,14 @@ class ScrollableFrame(Frame):
         self.frames.append(frame)
         frame.update()
         self.canv.create_window((self.margin, self.last_end), window=frame)
-        self.last_end += (self.spacing + frame.winfo_reqheight())
-        prev = self.canv['scrollregion']
-        print(prev[6])
-        self.canv['scrollregion'] = (0, 0, 0, int(prev[6]) + self.spacing + frame.winfo_reqheight())
+        self.last_end+=frame.winfo_reqheight()
+        # self.last_end += (self.spacing + frame.winfo_reqheight())
+        # total_occupied=len(self.frames)*frame.winfo_reqheight()
+        # # total_occupied=sum([x.winfo_reqheight() for x in self.frames])
+        # if total_occupied>self.canv.winfo_height():
+        #     prev = self.canv['scrollregion']
+        #     print(prev[6])
+        #     self.canv['scrollregion'] = (0, 0, 0, int(prev[6]) + self.spacing + frame.winfo_reqheight())
 
     def insert_frame_beg(self):
         pass
@@ -36,18 +40,25 @@ class ScrollableFrame(Frame):
         # -1 is for direction inversion
         self.canv.yview_scroll(-1 * (event.delta // 200), "units")
 
+    def remove_frame(self,frame_object):
+        for x in self.frames:
+            if x is frame_object:
+                x.destroy()
+                self.frames.remove(x)
 
-root = Tk()
-sr = ScrollableFrame(root, 100, 60)
-sr.pack()
-# Tk.update(fr)
-# print(fr.winfo_reqheight())
-for x in range(100):
-    fr = Frame()
-    label = Label(fr, text="Hello text")
-    label.pack()
-    entry = Entry(fr)
-    entry.pack()
-    sr.insert_frame_end(fr)
 
-root.mainloop()
+
+# root = Tk()
+# sr = ScrollableFrame(root, 100, 60)
+# sr.pack()
+# # Tk.update(fr)
+# # print(fr.winfo_reqheight())
+# for x in range(100):
+#     fr = Frame()
+#     label = Label(fr, text="Hello text")
+#     label.pack()
+#     entry = Entry(fr)
+#     entry.pack()
+#     sr.insert_frame_end(fr)
+#
+# root.mainloop()
